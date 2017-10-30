1589b7b3f15d702837bc1f182d62d9b92e4d08b7
Oleg Arbuzov <boozze@mail.ru>
2017-10-30 23:25:24 +0300

    Initial commit

---
+++ Find_file.py
@@ -0,0 +1,39 @@
+import os
+
+
+migrations = 'Migrations'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+m_dir = os.path.join(current_dir, migrations)             # генерируем абсолютный путь до Migrations и сохраняем в переменную m_dir
+
+def find_sql(list):
+    new_list = []
+    for name in list:
+        filename, file_extension = os.path.splitext(name)
+        if file_extension == ".sql":
+            new_list.append(name)
+    return new_list
+
+def find_text(s, list):
+    new_list = []
+    for name in list:
+        with open(join_file(name)) as f:
+            for str in f:
+                if s in str:
+                    new_list.append(name)
+                    break
+    return new_list
+
+def join_file(file_name):
+    return os.path.join(m_dir, file_name)
+
+
+if __name__ == '__main__':
+    os.chdir("..")          # Для проверки доп задания меняем директорию
+    names = find_sql(os.listdir(m_dir))
+    while(True):
+        s = input("Введите строку: ")
+        names = find_text(s, names)
+        for i in range(len(names)):
+            print(names[i])
+        print("Всего:",  len(names))
+
